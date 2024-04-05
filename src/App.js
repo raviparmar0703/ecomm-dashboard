f94fa6ca3afd858a2a2d039496550775d02a2719
Ravi Parmar <raviparmar@gmail.com>
2024-04-05 10:55:26 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import {Button} from 'react-bootstrap'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>ecomm project</h1>
+      <button>noraml button</button> 
+      <Button>bootstrap button</Button>
+    </div>
+  );
+}
+
+export default App;
